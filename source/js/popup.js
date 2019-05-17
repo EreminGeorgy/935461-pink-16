2c246c81d5540699cfaf5301775a3ee0a5351280
EreminGeorgy <eremingeorgy@yandex.ru>
2019-05-17 05:34:33 +0400

    1

--- source/js/popup.js
+++ source/js/popup.js
@@ -8,23 +8,23 @@ var surname = form.querySelector("[name=surname]");
 var email = form.querySelector("[name=email]");
 
 
-button.addEventListener("click", function (evt) {
+button.addEventListener("click", function(evt) {
   popupSuccess.classList.remove("modal__show");
   popupError.classList.remove("modal__show");
   overlay.classList.remove("modal__show");
 });
 
-form.addEventListener("submit", function (evt) {
+form.addEventListener("submit", function(evt) {
   overlay.classList.add("modal__show");
-  if (!surname.value || !givenName.value || !email.value) {  
+  if (!surname.value || !givenName.value || !email.value) {
     evt.preventDefault();
     popupError.classList.add("modal__show");
-  } else { 
+  } else {
     popupSuccess.classList.add("modal__show");
-    }
+  }
 });
 
-window.addEventListener("keydown",function(evt) {
+window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
     if (overlay.classList.contains("modal__show")) {
       evt.preventDefault();
